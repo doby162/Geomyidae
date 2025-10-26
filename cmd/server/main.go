24cb3fd81d01888ce444c0cadc381118088217ba
Michael Dorian <doby162@gmail.com>
2025-10-26 18:15:26 -0500

    import and use my higher order package for no reason

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"Geomyidae/cmd/server/sock_server"
+	"fmt"
+	"github.com/doby162/go-higher-order"
 )
 
 func main() {
+
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	b := higher_order.ReduceSlice(a, func(a, b int) int { return a + b })
+	fmt.Println(b)
+
 	err := sock_server.Api()
 	if err != nil {
 		return
--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/go-chi/chi/v5 v5.2.3
 	github.com/gorilla/websocket v1.5.3
 )
+
+require github.com/doby162/go-higher-order v0.1.1 // indirect
