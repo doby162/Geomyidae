86cbd8a66cb9a7df516a4ff354be14a1f8573c39
Michael Dorian <doby162@gmail.com>
2025-11-05 15:26:31 -0600

    gamestate organizes in it's own function

--- src/server/main.go
+++ src/server/main.go
@@ -22,6 +22,10 @@ type WorldData struct {
 	Objects []shared_structs.GameObject `json:"objects"`
 }
 
+var playerList *player.List
+var staticObjectList []*cp.Body
+var dynamicObjectList []*cp.Body
+
 func main() {
 	// Import tile data
 	tileByteInput, err := assets.FS.ReadFile("assets/tiled/test-one.tmx")
@@ -32,8 +36,7 @@ func main() {
 
 	// instantiate state
 	physics = cp.NewSpace()
-	playerList := player.NewList(physics)
-	var objectList []*cp.Body
+	playerList = player.NewList(physics)
 
 	for _, td := range tileData {
 		if td.ID == 0 {
@@ -48,7 +51,7 @@ func main() {
 		body.SetPosition(cp.Vector{X: float64(td.Col) + 0.5, Y: float64(td.Row) + 0.5})
 
 		physics.AddShape(shape)
-		objectList = append(objectList, body)
+		staticObjectList = append(staticObjectList, body)
 		physics.AddBody(body)
 	}
 
@@ -65,8 +68,22 @@ func main() {
 		prevTime = time.Now()
 		physics.Step(deltaTime)
 		playerList.WriteAccess.Unlock()
-		data := WorldData{}
-		for _, object := range objectList {
+
+		data := collectWorldState(true)
+
+		for sock, _ := range hub.Clients {
+			data.Name = sock.Player.Name
+			msg, _ := json.Marshal(data)
+			sock.Send <- msg
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
+}
+
+func collectWorldState(includeStaticAndAsleep bool) *WorldData {
+	data := WorldData{}
+	if includeStaticAndAsleep {
+		for _, object := range staticObjectList {
 			pos := object.Position()
 			x, y := pos.X, pos.Y
 			data.Objects = append(data.Objects, shared_structs.GameObject{
@@ -81,26 +98,39 @@ func main() {
 				Angle:   object.Angle(),
 			})
 		}
-		for _, networkPlayer := range playerList.Players {
-			pos := networkPlayer.Body.Position()
-			x, y := pos.X, pos.Y
-			data.Objects = append(data.Objects, shared_structs.GameObject{
-				X:       x,
-				Y:       y,
-				Sprite:  "spaceShooterRedux",
-				OffsetX: 325,
-				OffsetY: 0,
-				Width:   98,
-				Height:  75,
-				Name:    networkPlayer.Name,
-				Angle:   networkPlayer.Body.Angle(),
-			})
-		}
-		for sock, _ := range hub.Clients {
-			data.Name = sock.Player.Name
-			msg, _ := json.Marshal(data)
-			sock.Send <- msg
-		}
-		time.Sleep(10 * time.Millisecond)
 	}
+	for _, object := range dynamicObjectList {
+		if object.IsSleeping() && !includeStaticAndAsleep {
+			continue
+		}
+		pos := object.Position()
+		x, y := pos.X, pos.Y
+		data.Objects = append(data.Objects, shared_structs.GameObject{
+			X:       x,
+			Y:       y,
+			Sprite:  "platformerPack_industrial",
+			OffsetX: 0,
+			OffsetY: 0,
+			Width:   64,
+			Height:  64,
+			Name:    "",
+			Angle:   object.Angle(),
+		})
+	}
+	for _, networkPlayer := range playerList.Players {
+		pos := networkPlayer.Body.Position()
+		x, y := pos.X, pos.Y
+		data.Objects = append(data.Objects, shared_structs.GameObject{
+			X:       x,
+			Y:       y,
+			Sprite:  "spaceShooterRedux",
+			OffsetX: 325,
+			OffsetY: 0,
+			Width:   98,
+			Height:  75,
+			Name:    networkPlayer.Name,
+			Angle:   networkPlayer.Body.Angle(),
+		})
+	}
+	return &WorldData{}
 }
